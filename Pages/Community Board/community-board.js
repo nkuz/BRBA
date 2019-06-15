57f34b9cbaead2cb580cb125ba3fdd632b6fdb89
bethpol <bpolito001@gmail.com>
2019-06-14 22:56:53 -0400

    pins on imgs :pushpin:
    pins are now on all cards

--- Pages/Community Board/community-board.js
+++ Pages/Community Board/community-board.js
@@ -3,8 +3,8 @@
   $("#flyer3").draggable({ handle: ".pin"})
   $("#flyer4").draggable({ handle: ".pin"})
   $("#flyer10").draggable({ handle: ".pin"})
-  $("#flyer1").draggable()
-  $("#flyer100").draggable()
-  $("#flyer5").draggable()
+  $("#flyer1").draggable({ handle: ".pin"})
+  $("#flyer100").draggable({ handle: ".pin"})
+  $("#flyer5").draggable({ handle: ".pin"})
 
 })(jQuery);
