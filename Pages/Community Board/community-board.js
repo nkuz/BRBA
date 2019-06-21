d48e4104690c274bb049e0979c89c6309e5d7225
michael wu <michaelwu21@gmail.com>
2019-06-21 15:26:06 -0400

    Finish individual member page

---
+++ Pages/Community Board/community-board.js
@@ -0,0 +1,10 @@
+(function($) {
+
+  $("#flyer3").draggable({ handle: ".pin"})
+  $("#flyer4").draggable({ handle: ".pin"})
+  $("#flyer10").draggable({ handle: ".pin"})
+  $("#flyer1").draggable()
+  $("#flyer100").draggable()
+  $("#flyer5").draggable()
+
+})(jQuery);
